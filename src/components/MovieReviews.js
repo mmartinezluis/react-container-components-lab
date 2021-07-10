9b259bf8f12b62339e3be1e3bc67adf44c103b07
Luis Martinez <luis_mmartinez@live.com>
2021-07-10 19:56:53 -0400

    commit tree c2e767e04ee096bbee7d6df4f8f85a7f082bc8dc

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,10 +1,15 @@
 // Code MovieReviews Here
 import React from 'react'
 
-const MovieReviews = props => 
-    <div className="review-list">
-        {props.reviews.map((review,index) => <div key={index} className="review">{review}</div> ) }
-    </div>
+const MovieReviews = props => {
+    return(
+        <div className="review-list">
+            {props.reviews.map((review,index) => (
+                <div key={index} className="review">{review}</div> 
+            ))}
+        </div>
+    )
+}
 
         
 export default MovieReviews
